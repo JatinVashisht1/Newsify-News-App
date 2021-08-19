9c8bec350294de52dd4fb223ea7e462ad72572b8
JatinVashisht1 <jatin8085vashisht@gmail.com>
2021-08-19 13:07:21 +0530

    Name: Second Commit Bug Fix: null Features Added: null Files Changed: 1.MainActivity.kt linked the HomeScreenFragment.kt with MainActivity.kt file 2. themes.xml(dark and light) removed default action bar, we will use custom action bar(or at least try to)

--- app/src/main/java/com/example/newsify/Fragments/HomeScreenFragment.kt
+++ app/src/main/java/com/example/newsify/Fragments/HomeScreenFragment.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MenuDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.example.newsify.ui.theme.NewsifyTheme
 
@@ -22,11 +30,31 @@ class HomeScreenFragment : Fragment() {
                 .apply {
                    setContent {
                        NewsifyTheme {
-
-
-
+                           FirstScreen()
                        }
                    }
                 }
+    }
+}
+
+@Composable
+fun FirstScreen() {
+    Column(
+
+    ) {
+        TopAppBar(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+
+        ) {
+            Text("Newsify", fontSize = 20.sp, color = Color.White)
+        }
+
+        Card(
+            modifier = Modifier
+                .padding(8.dp)
+        ){
+            Text("Newsify", color = Color.White)
+        }
     }
 }
--- app/src/main/java/com/example/newsify/MainActivity.kt
+++ app/src/main/java/com/example/newsify/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.newsify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsify.Fragments.HomeScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, HomeScreenFragment()).commit()
     }
 }
